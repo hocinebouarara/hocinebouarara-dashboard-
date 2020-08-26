4dd229e423d47a528ae7792a45e897340c27d1f5
hocinebouarara <hocinbouarara08@gmail.com>
2020-08-27 00:39:12 +0100

    this class for links of view

---
+++ src/helpers/Links.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author hocin
+ */
+public class Links {
+    
+    public static String HOMEVIEW = "/home/homeView.fxml";
+    public static String HOMECONTENT ="/home/homeContent.fxml";
+    public static String PRODUCTSVIEW = "/products/productsView.fxml";
+    public static String CLENTSVIEW = "/clients/clientView.fxml";
+    public static String SUPPLIERSVIEW = "/suppliers/suppliersView.fxml";
+    public static String PURCHASESVIEW = "/purchases/purchasesView.fxml";
+    public static String SALESVIEW = "/sales/salesView.fxml";
+    public static String STYLE ="/home/style.css";
+    
+}
